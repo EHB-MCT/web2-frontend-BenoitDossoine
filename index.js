173ba367647dae09f09361a3da4bb094740026d2
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-12-07 21:40:20 +0100

    Add eventlistener to add button

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ setTimeout(() => {
         loader.remove();
     }, 1100);
 }, Math.random() * (4000 - 2000) + 2000);
+
+window.onload = function () {
+    console.log("Window loaded!");
+
+    //Add eventlisteners to buttons on homepage
+    document.getElementById("addGameNight").addEventListener("click", (e) => {
+        console.log("Game night added!")
+    })
+}
